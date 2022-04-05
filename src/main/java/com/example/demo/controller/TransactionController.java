46316882547f03f866126af66403fa94ad0ef204
renato-castillo <renatocastillopingo@gmail.com>
2022-04-05 17:07:01 -0500

    modified yml

--- src/main/java/com/example/demo/controller/TransactionController.java
+++ src/main/java/com/example/demo/controller/TransactionController.java
@@ -1,4 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Transaction;
+import com.example.demo.service.ITransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/transactions")
 public class TransactionController {
+
+    @Autowired
+    private ITransactionService transactionService;
+
+    @PostMapping
+    public Mono<Transaction> createRequest(@RequestBody Transaction transaction) {
+        return transactionService.create(transaction);
+    }
+
 }
